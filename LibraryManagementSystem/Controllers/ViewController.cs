5da595786797327422f4324cb7b802262e9465e8
Salma Magdy Elsayed Ahmed <salma.m.ishakk@gmail.com>
2025-05-20 15:34:26 +0300

    Add: Controllers add , Layout changed, View and Add Razor Pages

---
+++ LibraryManagementSystem/Controllers/ViewController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class ViewController : Controller
+    {
+        public IActionResult ViewAuthor()
+        {
+
+            return View();
+        }
+
+        public IActionResult ViewBook()
+        {
+            return View();
+
+        }
+
+           
+    }
+}
