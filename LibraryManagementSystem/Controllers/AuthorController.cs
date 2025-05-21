f7c7094444712722490e81f29daf351144101ebd
Salma Magdy Elsayed Ahmed <salma.m.ishakk@gmail.com>
2025-05-21 14:53:24 +0300

    Add: Author added Controller and Views accordingly (CRUD)

---
+++ LibraryManagementSystem/Controllers/AuthorController.cs
@@ -0,0 +1,83 @@
+﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AuthorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> ViewAuthor()
+        {
+            var authors = await _context.authors.ToListAsync();
+            return View("Author",authors);
+        }
+        [HttpGet]
+        public IActionResult AddAuthor()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddAuthor(Author author)
+        {
+                _context.Add(author);
+                _context.SaveChanges();
+              
+            var auth = _context.authors.ToList();
+            return View("Author", auth);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteAuthor(int ID)
+        {
+            var author = await _context.authors.FindAsync(ID);
+
+            if (author != null)
+            {
+                _context.authors.Remove(author);
+                await _context.SaveChangesAsync();
+            }
+
+
+            var authors = await _context.authors.ToListAsync();
+            return View("Author", authors);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditAuthorGet(int id)
+        {
+            var author = await _context.authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NoContent();
+            }
+
+            return View("EditAuthor",author); 
+        }
+
+        
+
+        [HttpPost]
+        public async Task<IActionResult> EditAuthor(Author author)
+        {
+             _context.authors.Update(author);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("ViewAuthor"); 
+           
+
+              
+        }
+
+
+
+
+
+    }
+}
---
+++ LibraryManagementSystem/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+
+            _context.Add(book);
+            _context.SaveChanges();
+            return View("Book",book);
+
+        }
+            
+
+        public async Task<IActionResult> ViewBook()
+        {
+            var books = await _context.books.ToListAsync();
+            return View("Book",books);
+
+        }
+
+
+
+
+    }
+}
